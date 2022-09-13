cc29f2c715a76594110bdd9f22ea85a6c3e49ab5
main <shapilov04@mail.ru>
2022-09-13 21:11:28 +0300

    fix bug

--- client.py
+++ client.py
@@ -5,10 +5,9 @@ sock = socket.socket()
 server_address = ('', 9090)
 
 
-def _connect() -> None:
+def _connect():
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM):
-            sock.connect(server_address)
+        sock.connect(server_address)
     except ConnectionRefusedError:
         print('Connect error')
         exit(1)
@@ -20,13 +19,17 @@ def _send_massage() -> None:
         if not massage:
             sock.close()
             break
-        sock.send(massage.encode('UTF-8'))
+        sock.sendall(massage.encode('UTF-8'))
         data = sock.recv(1024)
-        print(data)
+        print(data.decode())
+
+
+def main() -> None:
+    _connect()
+    _send_massage()
 
 
 if __name__ == '__main__':
-    _connect()
-    _send_massage()
+    main()
 
 
--- server.py
+++ server.py
@@ -8,19 +8,22 @@ class Server:
         self.all_clients = []
 
     def _append_client_to_list(self, writer: asyncio.StreamWriter) -> None:
+        """ add new connect to list connections """
         self.all_clients.append(writer)
 
     async def _send_message(self, writer: asyncio.StreamWriter, message: bytes, addr) -> None:
-        """ send message l  """
+        """ send message to list connections"""
         host, port = addr
+        msg = message.decode()
         for i in self.all_clients:
             if i != writer:
-                i.write(f'[Message port {host}]: {message}')
+                i.write(f'[Message from {host}: {msg}]'.encode('UTF-8'))
             else:
                 i.write(b'No massage')
 
     @staticmethod
     def _get_connect_info(writer: asyncio.StreamWriter) -> tuple:
+        """ get (host, port) connection """
         return writer.get_extra_info('peername')
 
     async def _server_read(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
@@ -40,6 +43,7 @@ class Server:
         writer.close()
 
     async def main(self) -> None:
+        """ starting server """
         start_server = await asyncio.start_server(
             self._server_read, server_host, server_port)
 
